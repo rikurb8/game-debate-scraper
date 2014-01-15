81237beae9e8bfc5fb4775bc78a586e2c48a9b3f
rikujjs <marttila.riku@gmail.com>
2014-01-15 13:23:32 +0200

    Added a general scraper file base

--- scraper.py
+++ scraper.py
@@ -8,11 +8,18 @@ from bs4 import BeautifulSoup
 import requests
 from time import sleep
 
+#column_headers array contains the names for the columns.
+column_headers = ["Title", "Releasedate"]
+
 
 class Scraper:
 
     #make the parsed soup in to a variable called "soup"
     def __init__(self, url):
+
+        #initialize the datastorage[] dict
+        for header in column_headers:
+            self.datastorage[header] = '-'
 
         #include possible spoofed header, and get the page
         header = {'User-Agent': 'Mozilla/5.0'}
@@ -21,15 +28,34 @@ class Scraper:
         #make some soup
         self.soup = BeautifulSoup(r.text.encode('utf-8', 'ignore'))
 
+    #we go thru the datastorage[] dict and write the info to a string, separated with ';'
+    def info_to_string(self):
+
+        info_string = ""
+
+        #write the columns to a single string, place a ';' in between
+        for header in column_headers:
+
+            info_string += ';'
+            info_string += self.datastorage[header].strip().encode('utf-8', 'ignore').replace(";", '')
 
 
+        #get rid of the first ;
+        info_string = info_string.replace(";", "", 1)
 
+        return info_string
 
+    #do the actual scraping
+    def get_pageinfo(self):
 
+        #do the actual scraping here
+
+        #after all the info is in the datastorage[] dictionary, we write it to a single string and return
+        return self.info_to_string()
 
     #member variables
     soup = None
-
+    datastorage = {}
 
 
 if __name__ == "__main__":
